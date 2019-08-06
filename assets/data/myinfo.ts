a95036ec774817bd4f144d886364fbc8493edd9f
PatrickKann <52541434+PatrickKann@users.noreply.github.com>
2019-08-06 11:07:02 -0300

    Add files via upload

---
+++ assets/data/myinfo.ts
@@ -0,0 +1,9 @@
+import { Kanch } from '../../app/kanch';
+
+export const MYINFO: Kanch = {
+  number: 991477200,
+  name: 'Patrick Kan',
+  login: 'Kanch',
+  campus: 'Traf',
+  assignment: 'Assignment 4'
+}
